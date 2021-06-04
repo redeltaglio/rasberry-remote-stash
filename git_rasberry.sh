1c566c90701d86e787e88d2e82c08b97e071fa15
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-06-04 18:48:35 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-04062021
     Your branch is up to date with 'origin/taglio-04062021'.
    
     Changes to be committed:
    	new file:   git_rasberry.sh

---
+++ git_rasberry.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DATE=$(date +%d%m%Y)
+DATE_RELEASE=$(date +"%d/%m/%Y %H:%m:%S")
+HOMEWRK="/home/riccardo/Work/redama"
+REPO="/rasberry-hackrf"
+RELEASE="/rasberryhackrf$DATE.tar"
+
+echo "creating tar release"
+rm -rf "$HOMEWRK$RELEASE"
+tar --exclude="$HOMEWRK$REPO/.git/*" -cvf "$HOMEWRK$RELEASE" "$HOMEWRK$REPO"
+
+echo "git add, commit, sign and push"
+cd "$HOMEWRK$REPO"
+echo "check branch"
+BRANCHCTRL=$(git branch | grep $DATE)
+if [ -z "${BRANCHCTRL}" ]
+then
+	git checkout -b taglio-$DATE
+	git push --set-upstream origin taglio-$DATE
+fi	
+git add .
+git commit -S
+git push --force
